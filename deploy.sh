92aeb01b2d62501ba20a2dca7e5eb5690762538d
程广 <chengguang@heytea.com>
2020-01-09 13:12:02 +0800

    123

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,7 @@
  # @Description: 
  # @Author: 程广
  # @Date: 2020-01-08 22:07:14
- # @LastEditTime : 2020-01-09 13:03:06
+ # @LastEditTime : 2020-01-09 13:11:47
  ###
 #!/usr/bin/env sh
 
@@ -23,7 +23,7 @@ git commit -m 'deploy'
 # git push -f https://${wmIuv87hcrm2AUmtGez5VA}@github.com/<ChengGPBU>/<ChengGPBU>.github.io.git master
 
 # 如果发布到 https://<USERNAME>.github.io
-git push -f git@github.com:<ChengGPBU>/<ChengGPBU>.github.io.git master
+git push -f git@github.com:ChengGPBU/ChengGPBU.github.io.git master
 
 
 cd -
