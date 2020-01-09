b481b4485c0d38cc343bf46f3ebe8130f6cd77a2
程广 <chengguang@heytea.com>
2020-01-09 14:10:49 +0800

    1.adaf

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,7 @@
  # @Description: 
  # @Author: 程广
  # @Date: 2020-01-08 22:07:14
- # @LastEditTime : 2020-01-09 13:11:47
+ # @LastEditTime : 2020-01-09 14:10:30
  ###
 #!/usr/bin/env sh
 
@@ -20,10 +20,9 @@ git init
 git add -A
 git commit -m 'deploy'
 
-# git push -f https://${wmIuv87hcrm2AUmtGez5VA}@github.com/<ChengGPBU>/<ChengGPBU>.github.io.git master
+git push -f https://${access_token}@github.com/ChengGPBU/ChengGPBU.github.io.git master
 
 # 如果发布到 https://<USERNAME>.github.io
-git push -f git@github.com:ChengGPBU/ChengGPBU.github.io.git master
-
+# git push -f git@github.com:ChengGPBU/ChengGPBU.github.io.git master
 
 cd -
