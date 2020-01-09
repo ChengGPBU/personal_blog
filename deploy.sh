397182f48c7ff05bd6dfd810033f4b29ef072554
程广 <chengguang@heytea.com>
2020-01-09 12:44:34 +0800

    1.修改文件

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,10 @@
+###
+ # @Company: heytea
+ # @Description: 
+ # @Author: 程广
+ # @Date: 2020-01-08 22:07:14
+ # @LastEditTime : 2020-01-09 12:44:03
+ ###
 #!/usr/bin/env sh
 
 # 确保脚本抛出遇到的错误
@@ -14,10 +21,10 @@ git add -A
 git commit -m 'deploy'
 
 #
-# git push -f https://${wmIuv87hcrm2AUmtGez5VA}@github.com/<ChengGPBU>/<ChengGPBU>.github.io.git master
+git push -f https://${wmIuv87hcrm2AUmtGez5VA}@github.com/<ChengGPBU>/<ChengGPBU>.github.io.git master
 
 # 如果发布到 https://<USERNAME>.github.io
-git push -f git@github.com:<ChengGPBU>/<ChengGPBU>.github.io.git master
+# git push -f git@github.com:<ChengGPBU>/<ChengGPBU>.github.io.git master
 
 
 cd -
